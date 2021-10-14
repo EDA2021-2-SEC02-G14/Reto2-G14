0ebbda1ecadb4874e82d63eaf4645ec16354a82d
Daniel Lozano <j.lozanom@uniandes.edu.co>
2021-10-13 20:51:26 -0500

    prueba

--- App/controller.py
+++ App/controller.py
@@ -24,15 +24,64 @@ import config as cf
 import model
 import csv
 
-
 """
 El controlador se encarga de mediar entre la vista y el modelo.
 """
 
 # Inicialización del Catálogo de libros
+def initCatalog():
+    """
+    Llama la funcion de inicializacion del catalogo del modelo.
+    """
+    catalog = model.newCatalog()
+    return catalog
 
 # Funciones para la carga de datos
+def loadData(catalog):
+    """
+    Carga los datos de los archivos y cargar los datos en la
+    estructura de datos
+    """
+    loadObras(catalog)
+    loadArtistas(catalog)
+    loadNacionalidades(catalog)
+
+def loadObras(catalog):
+    archivo_obras = cf.data_dir + 'Artworks-utf8-small.csv'
+
+    input_file = csv.DictReader(open(archivo_obras, encoding='utf-8'))
+
+    for obra in input_file:
+        model.addObra(catalog,obra)
+
+def loadArtistas(catalog):
+    archivo_Artistas = cf.data_dir + 'Artists-utf8-small.csv'
+
+    input_file = csv.DictReader(open(archivo_Artistas, encoding='utf-8'))
+
+    for artista in input_file:
+        model.addArtistas(catalog,artista)
+
+def loadNacionalidades(catalog):
+    archivo_naciones = cf.data_dir + 'Artworks-utf8-small.csv'
+
+    input_file = csv.DictReader(open(archivo_naciones, rncoding = 'utf-8'))
+
+    for nacion in input_file:
+        model.addnacion(catalog,nacion)
+        
+
+
+
 
 # Funciones de ordenamiento
 
 # Funciones de consulta sobre el catálogo
+
+def obrasSize (catalog):
+
+    return model.obrasSize(catalog)
+
+def artistaSize (catalog):
+    
+    return model.artistaSize(catalog)
